04da717eed1c5587055fcdc02b496a783aef9225
Dominique Sakrisson <domsakrisson@gmailcom>
2021-08-05 18:38:13 -0700

    Fix for netlify deployment

--- src/services/newsApi.js
+++ src/services/newsApi.js
@@ -13,17 +13,17 @@ export const getArticles = async(search) => {
     }))
 }
 
-export const getHeadlines = async() => {
-    const res = await fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.NEWS_API_KEY}&pageSize=20`);
+// export const getHeadlines = async() => {
+//     const res = await fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.NEWS_API_KEY}&pageSize=20`);
     
-    const json = await res.json();
-    console.log(json);
+//     const json = await res.json();
+//     console.log(json);
   
-    return json.articles.map((article) => ({
-        author: article.author,
-        title: article.title,
-        description: article.description,
-        url: article.url,
-        urlToImage: article.urlToImage
-    }))
-}
+//     return json.articles.map((article) => ({
+//         author: article.author,
+//         title: article.title,
+//         description: article.description,
+//         url: article.url,
+//         urlToImage: article.urlToImage
+//     }))
+// }
